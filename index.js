015d9c9ddd9fb6d6ad93df2830f15c044dca91fe
TurjoSiam <t.siam24@gmail.com>
2025-01-18 15:00:38 +0600

    expressjs, mongodb, dotenv installed and env added

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k0g53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+
+
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("server is running")
+});
+
+app.listen(port, () => {
+    console.log(`server is running in ${port}`);
+})
